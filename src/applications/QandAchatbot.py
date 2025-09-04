36249956267651bd41f1f910fcdab09d6a9e5089
lokesh <lokesh.saini@dianapps.com>
2025-09-04 15:20:25 +0530

    genai-sdk and some projects

---
+++ src/applications/QandAchatbot.py
@@ -0,0 +1,43 @@
+from dotenv import load_dotenv
+
+load_dotenv()
+
+import streamlit as st
+import os
+from google import genai
+
+
+client=genai.Client(api_key=os.getenv("GOOGLE_API_KEY"))
+
+chat=client.chats.create(model="gemini-2.0-flash")
+
+def get_gemini_response(question):
+    response=chat.send_message_stream(question)
+    return response
+
+
+st.set_page_config(page_title="Q&A ChatBot")
+st.header("Gemini LLM application")
+
+#initialize session state for chat history if it doesn't exist
+
+if 'chat_history' not in st.session_state:
+    st.session_state['chat_history']=[]
+
+input=st.text_input("Input:", key="input")
+submit=st.button("Ask")
+
+if submit and input:
+    response=get_gemini_response(input)
+    ##adding user query and response to session chat history
+    st.session_state['chat_history'].append(("You",input))
+    st.subheader("The response is")
+
+    for chunk in response:
+        st.write(chunk.text)
+        st.session_state['chat_history'].append(("LLM",chunk.text))
+
+    st.subheader("Chat history is")
+
+for role,text in st.session_state['chat_history']:
+    st.write(f"{role}:{text}")
---
+++ src/applications/vision.py
@@ -0,0 +1,39 @@
+from dotenv import load_dotenv
+
+load_dotenv()
+
+import streamlit as st
+import os
+from google import genai
+from PIL import Image
+
+
+client=genai.Client(api_key=os.getenv("GOOGLE_API_KEY"))
+
+def get_gemini_response(input,image):
+    if input!="":
+        response=client.models.generate_content(model="gemini-2.5-flash-lite",contents=[input,image])
+    else:
+        response=client.models.generate_content(model="gemini-2.5-flash-lite",contents=image)
+        
+    return response.text
+
+st.set_page_config(page_title="Gemini image demo")
+
+st.header("Gemini Application")
+
+input=st.text_input("Input: ", key="input")
+
+uploaded_file=st.file_uploader("Choose an image",type=["jpg","jpeg","png"])
+image=""
+if uploaded_file is not None:
+    image=Image.open(uploaded_file)
+    st.image(image,caption="Uploaded image",width='stretch')
+
+
+submit=st.button("Info about image")
+
+##when submit is clicked 
+if submit:
+    response=get_gemini_response(input,image)
+    st.write(response)
