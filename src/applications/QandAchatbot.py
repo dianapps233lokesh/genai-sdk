726b7804cc6d5fbc52244110c57777e8ebbc6200
lokesh <lokesh.saini@dianapps.com>
2025-09-09 17:51:35 +0530

    langGraph with multiple tools and output parser implemented

--- src/applications/QandAchatbot.py
+++ src/applications/QandAchatbot.py
@@ -7,37 +7,38 @@ import os
 from google import genai
 
 
-client=genai.Client(api_key=os.getenv("GOOGLE_API_KEY"))
+client = genai.Client(api_key=os.getenv("GOOGLE_API_KEY"))
 
-chat=client.chats.create(model="gemini-2.0-flash")
+chat = client.chats.create(model="gemini-2.0-flash")
+
 
 def get_gemini_response(question):
-    response=chat.send_message_stream(question)
+    response = chat.send_message_stream(question)
     return response
 
 
 st.set_page_config(page_title="Q&A ChatBot")
 st.header("Gemini LLM application")
 
-#initialize session state for chat history if it doesn't exist
+# initialize session state for chat history if it doesn't exist
 
-if 'chat_history' not in st.session_state:
-    st.session_state['chat_history']=[]
+if "chat_history" not in st.session_state:
+    st.session_state["chat_history"] = []
 
-input=st.text_input("Input:", key="input")
-submit=st.button("Ask")
+input = st.text_input("Input:", key="input")
+submit = st.button("Ask")
 
 if submit and input:
-    response=get_gemini_response(input)
+    response = get_gemini_response(input)
     ##adding user query and response to session chat history
-    st.session_state['chat_history'].append(("You",input))
+    st.session_state["chat_history"].append(("You", input))
     st.subheader("The response is")
 
     for chunk in response:
         st.write(chunk.text)
-        st.session_state['chat_history'].append(("LLM",chunk.text))
+        st.session_state["chat_history"].append(("LLM", chunk.text))
 
     st.subheader("Chat history is")
 
-for role,text in st.session_state['chat_history']:
+for role, text in st.session_state["chat_history"]:
     st.write(f"{role}:{text}")
